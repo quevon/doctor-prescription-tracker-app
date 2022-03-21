fb40ea2ae49632cdf70c346f08305f4d7979150f
quevon <quevonmontojo@gmail.com>
2022-03-21 20:13:29 +0800

    adding create new record in prescription

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -3,20 +3,20 @@ class PrescriptionsController < ApplicationController
     @doctor = Doctor.find(params[:doctor_id])
     @prescriptions = @doctor.prescriptions
   end
-  # def new
-  #     @doctor = Doctor.find(params[:doctor_id])
-  #     @prescription = @doctor.prescriptions.build
-  # end
-  # def create
-  #     @doctor = Doctor.find(params[:doctor_id])
-  #     @prescription = @doctor.prescriptions.build(prescription_params)
+  def new
+      @doctor = Doctor.find(params[:doctor_id])
+      @prescription = @doctor.prescriptions.build
+  end
+  def create
+      @doctor = Doctor.find(params[:doctor_id])
+      @prescription = @doctor.prescriptions.build(prescription_params)
 
-  #     if @comment.save
-  #         redirect_to doctor_prescriptions_path(@doctor.id)
-  #     else
-  #         render :new
-  #     end
-  # end
+      if @prescription.save
+          redirect_to doctor_prescriptions_path(@doctor.id)
+      else
+          render :new
+      end
+  end
   # def destroy
   #     @article = Article.find(params[:article_id])
   #     @comment = Comment.find(params[:id])
@@ -24,9 +24,9 @@ class PrescriptionsController < ApplicationController
   #     redirect_to article_comments_path(@article.id)
   # end
 
-  # private
+  private
 
-  # def prescription_params
-  #     params.require(:prescription).permit(:prescription_date,:doctor_id)
-  # end
+  def prescription_params
+      params.require(:prescription).permit(:prescription_date,:doctor_id)
+  end
 end
