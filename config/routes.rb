abf131993a14983c193c98000516015e17fdb548
quevon <quevonmontojo@gmail.com>
2022-03-21 18:33:26 +0800

    adding prescription models and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
   resources :drugs
-  resources :doctors
+  resources :doctors do
+    resources :prescriptions
+  end
 
   root to: "doctors#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_20_001100) do
+ActiveRecord::Schema.define(version: 2022_03_21_102827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,4 +31,13 @@ ActiveRecord::Schema.define(version: 2022_03_20_001100) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "prescriptions", force: :cascade do |t|
+    t.date "prescription_date"
+    t.bigint "doctor_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["doctor_id"], name: "index_prescriptions_on_doctor_id"
+  end
+
+  add_foreign_key "prescriptions", "doctors"
 end
