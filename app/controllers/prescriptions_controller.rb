06f9fa90d4139525a0e9b164062866ab010e4d42
quevon <quevonmontojo@gmail.com>
2022-03-21 19:59:23 +0800

    fixing errors in models

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -1,16 +1,32 @@
 class PrescriptionsController < ApplicationController
   def index
+    @doctor = Doctor.find(params[:doctor_id])
+    @prescriptions = @doctor.prescriptions
   end
+  # def new
+  #     @doctor = Doctor.find(params[:doctor_id])
+  #     @prescription = @doctor.prescriptions.build
+  # end
+  # def create
+  #     @doctor = Doctor.find(params[:doctor_id])
+  #     @prescription = @doctor.prescriptions.build(prescription_params)
 
-  def new
-  end
+  #     if @comment.save
+  #         redirect_to doctor_prescriptions_path(@doctor.id)
+  #     else
+  #         render :new
+  #     end
+  # end
+  # def destroy
+  #     @article = Article.find(params[:article_id])
+  #     @comment = Comment.find(params[:id])
+  #     @comment.destroy
+  #     redirect_to article_comments_path(@article.id)
+  # end
 
-  def create
-  end
+  # private
 
-  def edit
-  end
-
-  def destroy
-  end
+  # def prescription_params
+  #     params.require(:prescription).permit(:prescription_date,:doctor_id)
+  # end
 end
